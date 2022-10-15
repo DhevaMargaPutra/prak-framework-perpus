49312a38b4dc63811ea79cb5bac420d6446f4b9d
dhevamargaputra <dhevamp57@gmail.com>
2022-10-15 18:21:57 +0700

    view, route: info

---
+++ resources/views/info.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info Mahasiswa</title>
+</head>
+<body>
+    <h1>
+        Saya adalah mahasiswa program studi:
+        @if ($progdi === 'TI')
+            Teknik Informatika
+        @elseif ($progdi === 'SI')
+            Sistem Informasi
+        @elseif ($progdi === 'IK')
+            Ilmu Komunikasi
+        @else
+            Tidak ada progdi tersebut di Fakultas TIK 
+        @endif 
+    </h1>
+
+</body>
+</html>
