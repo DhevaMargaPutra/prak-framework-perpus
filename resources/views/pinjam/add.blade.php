b8fbc82559c1d1954d93439c09dce0029a69fb50
dhevamargaputra <dhevamp57@gmail.com>
2022-12-04 18:23:15 +0700

    module 5 draft2

--- resources/views/pinjam/add.blade.php
+++ resources/views/pinjam/add.blade.php
@@ -9,7 +9,28 @@
 </head>
 
 <body>
-
+    <h2>Form Pinjam Buku</h2>
+    <form action="{{ url('pinjam/save') }}" method="POST">
+        @csrf
+        Anggota: <select name="ID_Anggota">
+            <option value="">-Pilih Anggota-</option>
+            @foreach ($optanggota as $key => $value)
+                <option value="{{ $key }}">{{ $value }}</option>
+            @endforeach
+        </select>
+        <br><br>
+        Buku: <select name="ID_Buku">
+            <option value="">-Pilih Anggota-</option>
+            @foreach ($optbuku as $key => $value)
+                <option value="{{ $key }}">{{ $value }}</option>
+            @endforeach
+        </select>
+        <br><br>Tgl. Pinjam : <input type="date" name="tgl_pinjam">
+        <br><br>Tgl. Kembali : <input type="date" name="tgl_kembali">
+        <br><br><input type="submit" value="Simpan">
+        <input type="reset" value="Batal">
+    </form>
+    <br><a href="{{ url('pinjam') }}">Kembali</a>
 </body>
 
 </html>
